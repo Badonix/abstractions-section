5b8e0a5d90a90f1acc86e6ee3b07343a65bf86b4
Nick <nikdanelia@gmail.com>
2026-05-29 21:11:43 +0400

    fix: univalue pathdocs: problem links

---
+++ section8/README.md
@@ -0,0 +1,11 @@
+[Invert Binary Tree](https://leetcode.com/problems/invert-binary-tree/)
+
+[Symmetric Tree](https://leetcode.com/problems/symmetric-tree/)
+
+[Right Side View](https://leetcode.com/problems/binary-tree-right-side-view/)
+
+[Longest Univalue](https://leetcode.com/problems/longest-univalue-path/)
+
+[Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/)
+
+[Path Sum](https://leetcode.com/problems/path-sum)
--- section8/Univalue.cpp
+++ section8/Univalue.cpp
@@ -1,8 +1,29 @@
-/**
- *  NOT COMPLETE
- */
 class Solution {
 public:
+
+    // Correct version
+    // int dfs(TreeNode* root, int& answ){
+    //   if(!root) return 0;
+    //
+    //   int l = dfs(root->left, answ);
+    //   int r = dfs(root->right, answ);
+    //
+    //   int l_val = 0;
+    //   int r_val = 0;
+    //   if(root->left && root->left->val == root->val){
+    //     l_val = l + 1;
+    //   }
+    //
+    //   if(root->right && root->right->val == root->val){
+    //     r_val = r + 1;
+    //   }
+    //
+    //   answ = max(answ, l_val + r_val);
+    //
+    //   return max(l_val, r_val);
+    // }
+    //
+
 
     int dfs(TreeNode* root, int& answ){
       if(!root) return 0;
